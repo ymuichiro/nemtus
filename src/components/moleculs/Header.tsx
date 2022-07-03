a40ca644b945c7052001783fcbc72aeeda6f4993
yuichiro - frinker <47295014+ymuichiro@users.noreply.github.com>
2022-07-03 23:03:53 +0900

    [add] First commit

---
+++ src/components/moleculs/Header.tsx
@@ -0,0 +1,164 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useTheme } from "@mui/material/styles";
+import Drawer from "@mui/material/Drawer";
+import Container from "@mui/material/Container";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+
+export default function Header() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          width: "100%",
+          position: "fixed",
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: "20px",
+        }}
+      >
+        <Container
+          maxWidth="md"
+          style={{
+            position: "absolute",
+          }}
+        >
+          <AppBar
+            position="absolute"
+            style={{
+              backgroundColor: "rgba(255,255,255,0.8)",
+              color: theme.palette.text.primary,
+              borderRadius: "10px",
+              width: "calc(100% - 48px)",
+              left: 0,
+              right: 0,
+              margin: "auto",
+            }}
+          >
+            <Toolbar>
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                component="div"
+                sx={{ flexGrow: 1 }}
+              >
+                NEMTUS
+              </Typography>
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Button
+                  variant="text"
+                  style={{
+                    color: "black",
+                    paddingRight: "50px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  TOP
+                </Button>
+                <Button
+                  variant="text"
+                  style={{
+                    color: "black",
+                    paddingRight: "50px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  ABOUT
+                </Button>
+                <Button
+                  variant="text"
+                  style={{
+                    color: "black",
+                    paddingRight: "50px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  POLICY
+                </Button>
+              </div>
+
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={() => setOpen(!open)}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </Container>
+      </div>
+      <Drawer anchor={"left"} open={open} onClose={() => setOpen(!open)}>
+        <List>
+          {[
+            "ホーム",
+            "お知らせ",
+            "活動",
+            "会員",
+            "寄付",
+            "法人概要",
+            "その他",
+            "テックブログ",
+          ].map((item, index) => (
+            <ListItemButton
+              key={index}
+              divider
+              style={{ width: "70vh", maxWidth: "300px" }}
+            >
+              <ListItemText primary={item} />
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+    </React.Fragment>
+  );
+}
+
+/*
+
+  <AppBar
+          style={{
+            marginTop: "20px",
+            backgroundColor: "rgba(255,255,255,0.8)",
+            borderRadius: "10px",
+            color: theme.palette.text.primary,
+          }}
+        >
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              NEMTUS
+            </Typography>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={() => setOpen(!open)}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
+*/
---
+++ src/pages/Top.tsx
@@ -0,0 +1,182 @@
+import React, { useEffect } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Header from "../components/moleculs/Header";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import LOGO_IMAGE from "../assets/logo.png";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Toolbar from "@mui/material/Toolbar";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import P1_IMAGE from "../assets/p1.png";
+
+export default function Top() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.between("xs", "md"));
+
+  useEffect(() => {
+    console.log(matches);
+  }, [matches]);
+
+  return (
+    <div className="App">
+      <CssBaseline>
+        <Header />
+        <div
+          style={{
+            backgroundColor: theme.palette.grey["900"],
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="md" style={{ height: "100%" }}>
+            <Grid container style={{ height: "100%" }}>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                style={{
+                  height: matches ? "30vh" : "60vh",
+                  display: "flex",
+                  alignItems: matches ? "flex-end" : "center",
+                }}
+              >
+                <div style={{ width: "100%" }}>
+                  <Typography
+                    color="white"
+                    variant="body1"
+                    align={matches ? "center" : "left"}
+                  >
+                    NPO法人 NEM技術普及推進会
+                  </Typography>
+                  <br />
+                  <Typography
+                    color="white"
+                    variant="h1"
+                    fontWeight={"bold"}
+                    align={matches ? "center" : "left"}
+                    style={{ fontSize: "2em" }}
+                  >
+                    NEMTUS
+                  </Typography>
+                </div>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                md={6}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  paddingTop: "50px",
+                  paddingBottom: "100px",
+                }}
+              >
+                <img src={LOGO_IMAGE} height="200px" width="200px" />
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
+        <Container maxWidth="md" style={{ position: "relative", top: "-50px" }}>
+          <Card
+            style={{
+              backgroundColor: "rgba(255,255,255,0.8)",
+              backdropFilter: "saturate(200%) blur(30px)",
+            }}
+          >
+            <CardContent>
+              <Typography
+                color="primary"
+                align="center"
+                variant="h4"
+                fontWeight="bold"
+                textAlign="center"
+                style={{ paddingTop: "50px", paddingBottom: "50px" }}
+              >
+                NEMTUSとは
+              </Typography>
+              <Container maxWidth="sm">
+                <Typography align="left" style={{ paddingBottom: "50px" }}>
+                  NEMTUS（NEM+）は、日本においてNEM（ネム）技術の普及や発展を促進するNPO法人です。「ねむたす」と読みます。
+                  NEMとは、海外の有志開発者によって2015年に誕生した、オープンソースのコミュニティ指向のブロックチェーン技術です。NEMという名前は「New
+                  Economy Movement」の頭文字からきています。
+                </Typography>
+              </Container>
+            </CardContent>
+            <CardContent>
+              <Typography
+                color="primary"
+                align="center"
+                variant="h4"
+                fontWeight="bold"
+                textAlign="center"
+                style={{ paddingTop: "50px", paddingBottom: "50px" }}
+              >
+                What's New
+              </Typography>
+            </CardContent>
+            <CardContent>
+              <Grid container spacing={5}>
+                {[
+                  {
+                    title:
+                      "「NEMTUS Hackathon Hack+2022 受賞式 in XYMCity」開催のお知らせ",
+                    image: P1_IMAGE,
+                    desc: "「NEMTUS Hackathon Hack+2022」のアフターイベント開催が決定したのでお知らせします。会場はオンライン、Cluster × Discordでライブ配信します！",
+                  },
+                  {
+                    title:
+                      "「NEMTUS Hackathon Hack+2022 受賞式 in XYMCity」開催のお知らせ",
+                    image: P1_IMAGE,
+                    desc: "「NEMTUS Hackathon Hack+2022」のアフターイベント開催が決定したのでお知らせします。会場はオンライン、Cluster × Discordでライブ配信します！",
+                  },
+                  {
+                    title:
+                      "「NEMTUS Hackathon Hack+2022 受賞式 in XYMCity」開催のお知らせ",
+                    image: P1_IMAGE,
+                    desc: "「NEMTUS Hackathon Hack+2022」のアフターイベント開催が決定したのでお知らせします。会場はオンライン、Cluster × Discordでライブ配信します！",
+                  },
+                  {
+                    title:
+                      "「NEMTUS Hackathon Hack+2022 受賞式 in XYMCity」開催のお知らせ",
+                    image: P1_IMAGE,
+                    desc: "「NEMTUS Hackathon Hack+2022」のアフターイベント開催が決定したのでお知らせします。会場はオンライン、Cluster × Discordでライブ配信します！",
+                  },
+                ].map((item, index) => (
+                  <Grid item key={index} xs={12} sm={12} md={6}>
+                    <Card>
+                      <CardMedia
+                        component="img"
+                        height="280"
+                        image={item.image}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {item.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Lizards are a widespread group of squamate reptiles,
+                          with over 6,000 species, ranging across all continents
+                          except Antarctica
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </CardContent>
+          </Card>
+        </Container>
+      </CssBaseline>
+    </div>
+  );
+}
